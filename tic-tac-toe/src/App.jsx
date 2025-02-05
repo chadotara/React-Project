dc89db89cd65186c08471ca694bc9479edc7ec9b
sunny chadotara <chadotara.sunny27@gmail.com>
2025-02-05 16:14:16 +0530

    tictactoe game

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TicTacToe from './Components/Tictactor/Tictactoe'
+
+function App() {
+  return (
+    <div>
+       <TicTacToe/>
+
+
+    </div>
+  )
+}
+
+export default App
